e3fb28627d2b08b7f578c6ca71af3d759b8c647e
Abhijeet <abhijeet.dighe@infosys.com>
2025-01-27 11:37:10 +0530

    Added few more changes for table values

--- src/components/PureComponent/Tabs.js
+++ src/components/PureComponent/Tabs.js
@@ -119,7 +119,6 @@ class Tabs extends PureComponent {
               <option value="2023">2023</option>
               <option value="2024">2024</option>
               <option value="2025">2025</option>
-              <option value="2026">2026</option>
             </select>
           </label>
         </div>
